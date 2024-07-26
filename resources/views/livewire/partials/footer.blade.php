f4c1df689af0293ceb924d74292684b2b7629a53
srmaarnav <arnav6266@gmail.com>
2024-07-26 09:19:01 +0545

    merge with lotus with updated captcha

--- resources/views/livewire/partials/footer.blade.php
+++ resources/views/livewire/partials/footer.blade.php
@@ -11,51 +11,19 @@
 						</p>
 					</div>
 				</div>
-				<div class="col-lg-4  col-md-6 col-sm-6">
+				<div class="col-lg-3  col-md-6 col-sm-6">
 					<div class="single-footer-widget">
 						<h6>Newsletter</h6>
-						<p>Stay update with our latest</p>
-						<div class="" id="mc_embed_signup">
-
-							<form target="_blank" novalidate="true" action=""
-							 method="get" class="form-inline">
-
-								<div class="d-flex flex-row">
-
-									<input class="form-control" name="EMAIL" placeholder="Enter Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email '"
-									 required="" type="email">
-
-
-									<button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></button>
-									<div style="position: absolute; left: -5000px;">
-										<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value="" type="text">
-									</div>
-
-									<!-- <div class="col-lg-4 col-md-4">
-												<button class="bb-btn btn"><span class="lnr lnr-arrow-right"></span></button>
-											</div>  -->
-								</div>
-								<div class="info"></div>
-							</form>
-						</div>
+						
 					</div>
 				</div>
 				<div class="col-lg-3  col-md-6 col-sm-6">
-					<div class="single-footer-widget mail-chimp">
-						<h6 class="mb-20">Instragram Feed</h6>
-						<ul class="instafeed d-flex flex-wrap">
-							<li><img src="{{asset('home-page-assets/img/i1.jpg')}}" alt=""></li>
-							<li><img src="{{asset('home-page-assets/img/i2.jpg')}}" alt=""></li>
-							<li><img src="{{asset('home-page-assets/img/i3.jpg')}}" alt=""></li>
-							<li><img src="{{asset('home-page-assets/img/i4.jpg')}}" alt=""></li>
-							<li><img src="{{asset('home-page-assets/img/i5.jpg')}}" alt=""></li>
-							<li><img src="{{asset('home-page-assets/img/i6.jpg')}}" alt=""></li>
-							<li><img src="{{asset('home-page-assets/img/i7.jpg')}}" alt=""></li>
-							<li><img src="{{asset('home-page-assets/img/i8.jpg')}}" alt=""></li>
-						</ul>
+					<div class="single-footer-widget">
+						<h6>Newsletter</h6>
+						
 					</div>
 				</div>
-				<div class="col-lg-2 col-md-6 col-sm-6">
+				<div class="col-lg-3 col-md-6 col-sm-6">
 					<div class="single-footer-widget">
 						<h6>Follow Us</h6>
 						<p>Let us be social</p>
@@ -69,9 +37,9 @@
 				</div>
 			</div>
 			<div class="footer-bottom h-3 d-flex justify-content-center align-items-center flex-wrap">
-				<p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy; All rights reserved | <a href="/" wire:navigate target="_blank">Tech Tonic</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+				<p class="footer-text m-0">
+Copyright 2024 &copy; All rights reserved | <a href="/" wire:navigate target="_blank">Tech Tonic</a>
+
 </p>
 			</div>
 		</div>
