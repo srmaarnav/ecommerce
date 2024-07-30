96029777b347831233cd691bf369a1c6d29915a5
srmaarnav <arnav6266@gmail.com>
2024-07-30 21:35:00 +0545

    final code update and modifies .env

--- resources/views/mail/orders/placed.blade.php
+++ resources/views/mail/orders/placed.blade.php
@@ -1,7 +1,7 @@
 <x-mail::message>
 # Order Placed Successfully!
 
-Thank you for your order. YOur order number is: {{$order->id}}.
+Thank you for your order. Your order number is: {{$order->id}}.
 
 <x-mail::button :url="$url">
 View Order
